a275c0bed06466fdbbefccf8d46d7687537c87ab
maria <marialaurabastos842@gmail.com>
2022-11-25 10:28:08 -0300

    versao2

--- Aula01.js
+++ Aula01.js
@@ -1,6 +1,9 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write('Brasil 2 x 1 Servia \n');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h2>Brasil 2 x 1 Servia</h2> <br>');
+
+    res.write(Date().substring(16,24)+'<br>');
     res.end ('Bom dia!');
 
 }).listen(3000);
