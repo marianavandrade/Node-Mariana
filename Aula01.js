0a71dd3cbee5d2c5ca33a83272bfd69aa8e13e86
maria <marialaurabastos842@gmail.com>
2022-11-25 10:48:17 -0300

    versao3

--- Aula01.js
+++ Aula01.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var dia = require('./biblioteca')
 http.createServer(function (req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h2>Brasil 2 x 1 Servia</h2> <br>');
-
+    res.write('<h2>Brasil 2 x 1 Suiça</h2> <br>');
     res.write(Date().substring(16,24)+'<br>');
+    res.write('< style=color:green'+Data().substring(0,3));
+    res.writable(dia.diaDaSemana()+'</p><br>')
     res.end ('Bom dia!');
 
 }).listen(3000);
